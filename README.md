34f57842fa0ace9886e6559005b5fa58e3c6d56a
Richard O'Neil <richard@bytemares.com>
2015-02-08 18:49:45 -0400

    Updated with other extensions for a big test

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # boxstarter-dev-machine
 Boxstarter script for a development machine
+
+To kick it off, run the following line from a Powershell prompt:
+
+START http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/groovyghoul/boxstarter-dev-machine/master/boxstarter-dev-machine.ps1
+
+Thanks to http://blog.zerosharp.com/provisioning-a-new-development-machine-with-boxstarter/ for the initial script.
--- boxstarter-dev-machine.ps1
+++ boxstarter-dev-machine.ps1
@@ -1,5 +1,3 @@
-# Boxstarter script for blog post http://blog.zerosharp.com/provisioning-a-new-development-machine-with-boxstarter
-
 # Allow reboots
 $Boxstarter.RebootOk=$true
 $Boxstarter.NoPassword=$false
@@ -27,33 +25,85 @@ if (Test-PendingReboot) { Invoke-Reboot }
 cinstm VS2013SDK
 if (Test-PendingReboot) { Invoke-Reboot }
 
-cinstm DotNet3.5 # Not automatically installed with VS 2013. Includes .NET 2.0. Uses Windows Features to install.
+cinstm dotnet4.5 # Not automatically installed with VS 2013. Uses Windows Features to install.
 if (Test-PendingReboot) { Invoke-Reboot }
 
 # VS extensions
-Install-ChocolateyVsixPackage PowerShellTools http://visualstudiogallery.msdn.microsoft.com/c9eb3ba8-0c59-4944-9a62-6eee37294597/file/112013/6/PowerShellTools.vsix
-Install-ChocolateyVsixPackage WebEssentials2013 http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/31/WebEssentials2013.vsix
-Install-ChocolateyVsixPackage T4Toolbox http://visualstudiogallery.msdn.microsoft.com/791817a4-eb9a-4000-9c85-972cc60fd5aa/file/116854/1/T4Toolbox.12.vsix
-Install-ChocolateyVsixPackage StopOnFirstBuildError http://visualstudiogallery.msdn.microsoft.com/91aaa139-5d3c-43a7-b39f-369196a84fa5/file/44205/3/StopOnFirstBuildError.vsix
+#Install-ChocolateyVsixPackage PowerShellTools http://visualstudiogallery.msdn.microsoft.com/c9eb3ba8-0c59-4944-9a62-6eee37294597/file/112013/6/PowerShellTools.vsix
+#Install-ChocolateyVsixPackage WebEssentials2013 http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/31/WebEssentials2013.vsix
+#Install-ChocolateyVsixPackage T4Toolbox http://visualstudiogallery.msdn.microsoft.com/791817a4-eb9a-4000-9c85-972cc60fd5aa/file/116854/1/T4Toolbox.12.vsix
+#Install-ChocolateyVsixPackage StopOnFirstBuildError http://visualstudiogallery.msdn.microsoft.com/91aaa139-5d3c-43a7-b39f-369196a84fa5/file/44205/3/StopOnFirstBuildError.vsix
 
 # AWS Toolkit is now an MSI available here http://sdk-for-net.amazonwebservices.com/latest/AWSToolsAndSDKForNet.msi (no chocolatey package as of FEB 2014)
 # Install-ChocolateyVsixPackage AwsToolkit http://visualstudiogallery.msdn.microsoft.com/175787af-a563-4306-957b-686b4ee9b497
 
+# ===========================
+
+try {
+  Install-ChocolateyVsixPackage "PowerShell Tools for Visual Studio" "http://visualstudiogallery.msdn.microsoft.com/c9eb3ba8-0c59-4944-9a62-6eee37294597/file/112013/5/PowerShellTools.vsix"
+  Install-ChocolateyVsixPackage "Web Essentials 2013" "http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/36/WebEssentials2013.vsix"
+  Install-ChocolateyVsixPackage T4Toolbox http://visualstudiogallery.msdn.microsoft.com/791817a4-eb9a-4000-9c85-972cc60fd5aa/file/116854/1/T4Toolbox.12.vsix
+  Install-ChocolateyVsixPackage StopOnFirstBuildError http://visualstudiogallery.msdn.microsoft.com/91aaa139-5d3c-43a7-b39f-369196a84fa5/file/44205/3/StopOnFirstBuildError.vsix
+  Install-ChocolateyVsixPackage "Visual Studio Spell Checker" "http://visualstudiogallery.msdn.microsoft.com/a23de100-31a1-405c-b4b7-d6be40c3dfff/file/104494/2/VSSpellCheckerPackage.vsix"
+  Install-ChocolateyVsixPackage "Productivity Power Tools 2013" "http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/3/ProPowerTools.vsix"
+  Install-ChocolateyVsixPackage "StopOnFirstBuildError" "http://visualstudiogallery.msdn.microsoft.com/91aaa139-5d3c-43a7-b39f-369196a84fa5/file/44205/3/StopOnFirstBuildError.vsix"
+  Install-ChocolateyVsixPackage "SideWaffle Template Pack" "http://visualstudiogallery.msdn.microsoft.com/a16c2d07-b2e1-4a25-87d9-194f04e7a698/referral/110630"
+  Install-ChocolateyVsixPackage "Ref12" "http://visualstudiogallery.msdn.microsoft.com/f89b27c5-7d7b-4059-adde-7ccc709fa86e/file/125016/4/Ref12.vsix"
+  Install-ChocolateyVsixPackage "CodeMaid" "http://visualstudiogallery.msdn.microsoft.com/76293c4d-8c16-4f4a-aee6-21f83a571496/file/9356/23/CodeMaid_v0.7.0.vsix"
+  Install-ChocolateyVsixPackage "ClipboardDiff" "http://visualstudiogallery.msdn.microsoft.com/a7519ab0-6029-49f3-9243-a74d1718a5bb/file/49749/4/ClipboardDiff.vsix"
+  Install-ChocolateyVsixPackage "VSColorOutput" "http://visualstudiogallery.msdn.microsoft.com/f4d9c2b5-d6d7-4543-a7a5-2d7ebabc2496/file/63103/7/VSColorOutput.vsix"
+  Install-ChocolateyVsixPackage "Add New File" "https://visualstudiogallery.msdn.microsoft.com/3f820e99-6c0d-41db-aa74-a18d9623b1f3/file/140782/13/AddAnyFile.vsix"
+  Install-ChocolateyVsixPackage "NuGet Reference Switcher" "https://visualstudiogallery.msdn.microsoft.com/68878c27-110c-43ec-ae61-3ea3f7aae88c/file/140063/7/NuGetReferenceSwitcher.vsix"
+  Install-ChocolateyVsixPackage "Pretty Paste" "https://visualstudiogallery.msdn.microsoft.com/6a23234d-45f6-4212-bac3-f6d9bb08fb1e/file/97937/12/PrettyPaste.vsix"
+  Install-ChocolateyVsixPackage "Image Optimizer" "https://visualstudiogallery.msdn.microsoft.com/a56eddd3-d79b-48ac-8c8f-2db06ade77c3/file/38601/14/ImageOptimizer.vsix"
+  Install-ChocolateyVsixPackage "AutoHistory " "https://visualstudiogallery.msdn.microsoft.com/dfcb2438-180c-4f8a-983b-62d89e141fe3/file/122993/3/Microsoft.VisualStudio.AutoHistory.vsix"
+  Install-ChocolateyVsixPackage "File Nesting" "https://visualstudiogallery.msdn.microsoft.com/3ebde8fb-26d8-4374-a0eb-1e4e2665070c/file/123284/11/FileNesting.vsix"
+  Install-ChocolateyVsixPackage "Error Watcher" "https://visualstudiogallery.msdn.microsoft.com/a85f155f-b519-44a8-b56b-07611cf78393/file/136589/6/ErrorHighlighter.vsix"
+  Install-ChocolateyVsixPackage "SlowCheetah - XML Transforms" "https://visualstudiogallery.msdn.microsoft.com/69023d00-a4f9-4a34-a6cd-7e854ba318b5/file/55948/26/SlowCheetah.vsix"
+  Install-ChocolateyVsixPackage "Visual Studio Auto Updater" "https://visualstudiogallery.msdn.microsoft.com/14973bbb-8e00-4cab-a8b4-415a38d78615/file/150147/7/ExtensionUpdater.vsix"
+  Install-ChocolateyVsixPackage "CssCop - FxCop for Stylesheets" "https://visualstudiogallery.msdn.microsoft.com/a921b98e-9430-4be2-bf53-1169e12bdb50/file/62099/7/CssLint.vsix"
+  Install-ChocolateyVsixPackage "Open From Azure Websites" "https://visualstudiogallery.msdn.microsoft.com/60d414b1-4ead-4fde-9359-588aa126cd6c/file/151097/2/OpenFromPortal.vsix"
+  Install-ChocolateyVsixPackage "Trailing Whitespace Visualizer" "https://visualstudiogallery.msdn.microsoft.com/a204e29b-1778-4dae-affd-209bea658a59/file/135653/12/TrailingWhitespace.vsix"
+  Install-ChocolateyVsixPackage "GitHub Extension" "https://visualstudiogallery.msdn.microsoft.com/e4ba5ebd-bcd5-4e20-8375-bb8cbdd71d7e/file/148389/4/Alteridem.GitHub.Extension.vsix"
+  Install-ChocolateyVsixPackage "MVVM Light for VS2013" "https://visualstudiogallery.msdn.microsoft.com/2f214591-4162-4e41-9908-8bf88282968e/file/143801/4/MvvmLight.VS2013.vsix"
+  Install-ChocolateyVsixPackage "Open Command Line" "https://visualstudiogallery.msdn.microsoft.com/4e84e2cf-2d6b-472a-b1e2-b84932511379/file/151803/3/OpenCommandLine.vsix"
+  Install-ChocolateyVsixPackage "Tidy Tabs" "https://visualstudiogallery.msdn.microsoft.com/b80ab284-83f8-4022-bc78-95af126ba5f0/file/130014/4/TidyTabs.vsix"
+  Install-ChocolateyVsixPackage "YamlDotNetEditor" "https://visualstudiogallery.msdn.microsoft.com/34423c06-f756-4721-8394-bc3d23b91ca7/file/101669/8/YamlDotNetEditor.vsix"
+
+  Write-ChocolateySuccess 'bytemares.vs2013extensions'
+} catch {
+  Write-ChocolateyFailure 'bytemares.vs2013extensions' $($_.Exception.Message)
+  throw
+}
+
+# ===========================
+
+
+
+
+
 #Other dev tools
 cinstm fiddler4
-cinstm beyondcompare
-cinstm ProcExp #cinstm sysinternals
+cinstm winmerge
+cinstm ProcExp
+cinstm sysinternals
 cinstm NugetPackageExplorer
 cinstm windbg
 cinstm Devbox-Clink # Clink improves the existing command prompt (e.g., paste works)
-cinstm TortoiseHg
-#cinstm VisualHG # Chocolatey package is corrupt as of Feb 2014
 cinstm linqpad4
-cinstm TestDriven.Net
-cinstm ncrunch2.vs2013
-#cinstm dotpeek
-#cinstm InkScape
-#cinstm mssqlserver2012express
+cinstm git
+cinstm ConEmu
+cinstm poshgit
+cinstm SublimeText3
+cinstm dotpeek
+cinstm mssqlserver2014express
+cinstm mssqlservermanagementstudio2014express
+cinstm python
+cinstm nodejs
+cinstm RoboMongo
+#cinstm resharper
+#cintm SourceTree
+
 
 #Browsers
 cinstm googlechrome
@@ -63,6 +113,11 @@ cinstm firefox
 cinstm 7zip
 cinstm adobereader
 cinstm javaruntime
+cinstm greenshot
+cinstm truecrypt
+cinstm libreoffice
+cinstm IrfanView
+cinstm terminals
 
 #cinst Microsoft-Hyper-V-All -source windowsFeatures
 cinst IIS-WebServerRole -source windowsfeatures
